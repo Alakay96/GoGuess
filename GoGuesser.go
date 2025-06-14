4b085bfbdeb3e805fb8bf6ac2ff8e4dc4217dd29
Alex <alex_jj96@hotmail.com>
2025-06-14 20:31:45 +1000

    loops brother

--- GoGuesser.go
+++ GoGuesser.go
@@ -7,11 +7,35 @@ import (
 )
 
 func main() {
-	randInt(1, 100)
+	theNumber := randInt(1, 100)
+	var guess int
+	var numOfGuess int
 
-	fmt.Print("Welcome to GO Guessing game")
-	fmt.Print("Guess a number between 1 and 100")
-	fmt.Print("10 Guesses")
+	fmt.Println("Welcome to GO Guessing game")
+	fmt.Println("Guess a number between 1 and 100")
+	fmt.Println("10 Guesses")
+
+	fmt.Print("Enter your guess: ")
+	_, err := fmt.Scan(&guess)
+
+	for i := 0; i < 10; i++ {
+		if err != nil {
+			fmt.Println("Invalid input")
+		} else if guess < 0 || guess > 100 {
+			fmt.Printf("Invalid number, %d", guess)
+		} else {
+			if guess == theNumber {
+				fmt.Printf("You got it %d in %d", theNumber, numOfGuess)
+				break
+			} else {
+				fmt.Println("Incorrect")
+				numOfGuess += 1
+				fmt.Print("Enter your guess: ")
+				fmt.Scan(&guess)
+			}
+		}
+
+	}
 
 }
 
