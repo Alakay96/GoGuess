74cfee0cfba3793e6014634faa9b23aee737f2d2
Alex <alex_jj96@hotmail.com>
2025-06-14 17:06:32 +1000

    added rand int function

--- GoGuesser.go
+++ GoGuesser.go
@@ -1,13 +1,20 @@
+// https://roadmap.sh/projects/number-guessing-game
 package main
 
-import(
+import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
+	randInt(1, 100)
+
 	fmt.Print("Welcome to GO Guessing game")
 	fmt.Print("Guess a number between 1 and 100")
 	fmt.Print("10 Guesses")
-	
-	
+
+}
+
+func randInt(min, max int) int {
+	return min + rand.Intn(max-min)
 }
